525cbe7f4495d4063fcbc212686b5dcd487c7aa5
Yamasaki-pan961 <atusi08250522@gmail.com>
2021-11-13 16:28:25 +0900

    provider名と処理の変更

--- lib/providers/current_user_provider.dart
+++ lib/providers/current_user_provider.dart
@@ -3,19 +3,20 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:look_teacher/models/teacher_user_model.dart';
 
-final authUserStreamProvider = StreamProvider.autoDispose((ref) {
-  return FirebaseAuth.instance.authStateChanges();
+final authUserStreamProvider = StreamProvider<User?>((ref) {
+  return FirebaseAuth.instance.userChanges();
 });
 
-final currentUserProvider =
+final currentTeacherProvider =
     StateProvider<Future<TeacherUserModel?>>((ref) async {
-  final User? user = ref.read(authUserStreamProvider).data?.value;
+  ref.watch(authUserStreamProvider);
+  final User? user = FirebaseAuth.instance.currentUser;
   if (user != null) {
     final DocumentSnapshot userDoc = await FirebaseFirestore.instance
         .collection('users')
         .doc(user.uid)
         .get();
-    if (userDoc.exists) {
+    if (!userDoc.exists) {
       final teacher =
           TeacherUserModel(uid: user.uid, name: user.displayName ?? '');
       await FirebaseFirestore.instance
