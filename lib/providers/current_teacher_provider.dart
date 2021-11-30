59555d7e259fea006ac38b4c52479de3be3392c3
Yamasaki-pan961 <atusi08250522@gmail.com>
2021-12-01 08:58:14 +0900

    ログイン中のTeacherの所属するschoolを取得し、deviceIdListをローカルに保存

--- lib/providers/current_teacher_provider.dart
+++ lib/providers/current_teacher_provider.dart
@@ -5,18 +5,19 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:look_teacher/controller/school_crud_controller.dart';
 import 'package:look_teacher/controller/teacher_crud_controller.dart';
 import 'package:look_teacher/logic/background_process.dart';
 import 'package:look_teacher/logic/bluetooth_scan.dart';
+import 'package:look_teacher/models/school_model.dart';
 import 'package:look_teacher/models/teacher_user_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-final authUserStreamProvider = StreamProvider.autoDispose<User?>((ref) {
+final authUserStreamProvider = StreamProvider<User?>((ref) {
   return FirebaseAuth.instance.userChanges();
 });
 
-final currentTeacherStreamProvider =
-    StreamProvider.autoDispose<DocumentSnapshot?>((ref) {
+final currentTeacherStreamProvider = StreamProvider<DocumentSnapshot?>((ref) {
   final User? user = ref.watch(authUserStreamProvider).data?.value;
   if (user != null) {
     return TeacherCRUDController()
@@ -29,8 +30,8 @@ final currentTeacherStreamProvider =
 });
 
 Timer? scanTimer;
-final currentTeacherProvider =
-    StateProvider.autoDispose<TeacherUserModel?>((ref) {
+
+final currentTeacherProvider = StateProvider<TeacherUserModel?>((ref) {
   if (scanTimer != null) {
     scanTimer!.cancel();
   }
@@ -46,6 +47,7 @@ final currentTeacherProvider =
                     final preference = await SharedPreferences.getInstance();
                     final filterIdList =
                         preference.getStringList('filterIdList');
+                    log('filterIdList: $filterIdList');
                     if (filterIdList != null) {
                       final nearestDevice =
                           await getNearestDeviceId(filterIdList);
@@ -76,4 +78,37 @@ final currentTeacherProvider =
       },
       loading: () {},
       error: (object, stack) {});
+});
+
+final teacherSchoolStream = StreamProvider<DocumentSnapshot?>((ref) {
+  final teacher = ref.watch(currentTeacherProvider).state;
+  if (teacher != null) {
+    if (teacher.schoolId != '') {
+      return SchoolCRUDController()
+          .targetCollectionReference
+          .doc(teacher.schoolId)
+          .snapshots();
+    }
+  }
+  return Stream.value(null);
+});
+
+final teacherSchoolProvider = StateProvider<SchoolModel?>((ref) {
+  final stream = ref.watch(teacherSchoolStream);
+  return stream.when(
+      data: (value) {
+        if (value != null) {
+          final school = SchoolModel.fromDoc(value);
+          SharedPreferences.getInstance().then((value) {
+            value.setStringList(
+                'filterIdList',
+                school.deviceList.map((element) {
+                  return element.deviceId;
+                }).toList());
+          });
+          return school;
+        }
+      },
+      loading: () {},
+      error: (error, stackTrace) {});
 });
