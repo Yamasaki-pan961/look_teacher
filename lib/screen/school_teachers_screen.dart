550db829a5a3dde02e11bdaef8574759542b7261
YugeOnsen <rinta723@gmail.com>
2021-12-03 16:39:22 +0900

    iconの追加

--- lib/screen/school_teachers_screen.dart
+++ lib/screen/school_teachers_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:look_teacher/models/teacher_user_model.dart';
+import 'package:look_teacher/providers/favorite_provider.dart';
 import 'package:look_teacher/widget/teacher_widget.dart';
 
 class SchoolTeachersScreen extends HookWidget {
-  const SchoolTeachersScreen(
-      {Key? key, required this.teacherList})
+  const SchoolTeachersScreen({Key? key, required this.teacherList})
       : super(key: key);
   final List<TeacherUserModel> teacherList;
   @override
@@ -14,13 +15,14 @@ class SchoolTeachersScreen extends HookWidget {
       appBar: AppBar(
         title: const Text('教員一覧'),
       ),
-
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: listWidget(teacherList),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: listWidget(teacherList),
+            ),
           ),
         ),
       ),
@@ -30,7 +32,7 @@ class SchoolTeachersScreen extends HookWidget {
   List<Widget> listWidget(List<TeacherUserModel> teacherList) {
     final List<Widget> teacherWidget = [];
     for (final teacher in teacherList) {
-      teacherWidget.add(Expanded(child: TeacherWidget(teacherUserModel: teacher)));
+      teacherWidget.add(TeacherWidget(teacherUserModel: teacher));
     }
     return teacherWidget;
   }
--- lib/widget/teacher_widget.dart
+++ lib/widget/teacher_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:look_teacher/models/teacher_user_model.dart';
+import 'package:look_teacher/providers/favorite_provider.dart';
 
 class TeacherWidget extends HookWidget {
   const TeacherWidget({Key? key, required this.teacherUserModel})
@@ -11,17 +12,44 @@ class TeacherWidget extends HookWidget {
   final TeacherUserModel teacherUserModel;
   @override
   Widget build(BuildContext context) {
+    final favoriteTeacher = useProvider(favoriteTeacherIdListProvider);
+    bool isFavorite = false;
+    if (favoriteTeacher != null) {
+      isFavorite = favoriteTeacher.contains(teacherUserModel.uid);
+    }
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.blue,width: 2),
+        border: Border.all(color: Colors.blue, width: 2),
         borderRadius: BorderRadius.circular(10),
       ),
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical : 10.0, horizontal: 20.0),
-        
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
         child: Column(
           children: [
-            Text(teacherUserModel.name),
+            Row(
+              children: [
+                IconButton(
+                    onPressed: () {
+                      if (isFavorite) {
+                        final list = favoriteTeacher ?? []
+                          ..remove(teacherUserModel.uid);
+                        context
+                            .read(favoriteTeacherIdListProvider.notifier)
+                            .setState(list);
+                      } else {
+                        final list = favoriteTeacher ?? []
+                          ..add(teacherUserModel.uid);
+                        context
+                            .read(favoriteTeacherIdListProvider.notifier)
+                            .setState(list);
+                      }
+                    },
+                    icon: isFavorite
+                        ? Icon(Icons.star)
+                        : Icon(Icons.star_border_outlined)),
+                Text(teacherUserModel.name),
+              ],
+            ),
             Text(teacherUserModel.deviceId),
             Text(teacherUserModel.schoolId),
             Text(teacherUserModel.notifications.length.toString())
