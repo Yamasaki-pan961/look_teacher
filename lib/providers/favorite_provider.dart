766b1ed348f9a516357d13d5e73d6eb20a7d5373
Yamasaki-pan961 <atusi08250522@gmail.com>
2021-12-03 13:41:48 +0900

    favoriteSchoolMapProviderの作成

--- lib/providers/favorite_provider.dart
+++ lib/providers/favorite_provider.dart
@@ -1,4 +1,6 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:look_teacher/models/teacher_user_model.dart';
+import 'package:look_teacher/providers/schools_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final favoriteSchoolIdListProvider =
@@ -23,11 +25,28 @@ class FavoriteIdListNotifier extends StateNotifier<List<String>?> {
 
   Future<void> setState(List<String> list) async {
     {
-        if (state != list) {
-          await (await SharedPreferences.getInstance())
-              .setStringList(key, list);
-          state = list;
+      if (state != list) {
+        await (await SharedPreferences.getInstance()).setStringList(key, list);
+        state = list;
       }
     }
   }
 }
+
+final favoriteSchoolMapProvider = StateProvider<Map<String, String>>((ref) {
+  final favoriteSchoolIdList = ref.watch(favoriteSchoolIdListProvider);
+  final schoolMap = ref.watch(schoolMapProvider).state;
+  final favoriteSchoolMap = <String, String>{};
+  if (favoriteSchoolIdList != null && schoolMap != null) {
+    for (final schoolId in favoriteSchoolIdList) {
+      favoriteSchoolMap.addEntries({MapEntry(schoolId, schoolMap[schoolId]!)});
+    }
+  }
+  return favoriteSchoolMap;
+});
+
+final favoriteTeacherListProvider =
+    StateProvider<List<TeacherUserModel>?>((ref) {
+  final favoriteTeacherIdList = ref.watch(favoriteTeacherIdListProvider);
+  if (favoriteTeacherIdList != null) {}
+});
