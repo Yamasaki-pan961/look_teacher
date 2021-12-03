c94e5c9a93285a1120cd11b85db2cfe135d5f066
YugeOnsen <rinta723@gmail.com>
2021-12-03 15:33:00 +0900

    教員一覧のリストと表示の作成（providerは通してない)

--- lib/screen/school_teachers_screen.dart
+++ lib/screen/school_teachers_screen.dart
@@ -4,19 +4,23 @@ import 'package:look_teacher/models/teacher_user_model.dart';
 import 'package:look_teacher/widget/teacher_widget.dart';
 
 class SchoolTeachersScreen extends HookWidget {
-  const SchoolTeachersScreen({Key? key}) : super(key: key);
-
+  const SchoolTeachersScreen(
+      {Key? key, required this.teacherList})
+      : super(key: key);
+  final List<TeacherUserModel> teacherList;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('教員一覧'),
       ),
+
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Center(
           child: Column(
-            children: listWidget([TeacherUserModel(uid: 'uid', name: 'たけし')]),
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: listWidget(teacherList),
           ),
         ),
       ),
@@ -26,7 +30,7 @@ class SchoolTeachersScreen extends HookWidget {
   List<Widget> listWidget(List<TeacherUserModel> teacherList) {
     final List<Widget> teacherWidget = [];
     for (final teacher in teacherList) {
-      teacherWidget.add(TeacherWidget(teacherUserModel: teacher));
+      teacherWidget.add(Expanded(child: TeacherWidget(teacherUserModel: teacher)));
     }
     return teacherWidget;
   }
--- lib/widget/teacher_widget.dart
+++ lib/widget/teacher_widget.dart
@@ -6,16 +6,28 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:look_teacher/models/teacher_user_model.dart';
 
 class TeacherWidget extends HookWidget {
-  const TeacherWidget({Key? key, required this.teacherUserModel}) : super(key: key);
+  const TeacherWidget({Key? key, required this.teacherUserModel})
+      : super(key: key);
   final TeacherUserModel teacherUserModel;
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(children: [
-        Text(teacherUserModel.name),
-        Text(teacherUserModel.deviceId),
-        Text(teacherUserModel.schoolId),
-      ],),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blue,width: 2),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical : 10.0, horizontal: 20.0),
+        
+        child: Column(
+          children: [
+            Text(teacherUserModel.name),
+            Text(teacherUserModel.deviceId),
+            Text(teacherUserModel.schoolId),
+            Text(teacherUserModel.notifications.length.toString())
+          ],
+        ),
+      ),
     );
   }
 }
