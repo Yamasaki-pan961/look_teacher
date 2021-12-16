d0061c054aec013dc804a586fe5e432d487a1af2
Yamasaki-pan961 <atusi08250522@gmail.com>
2021-12-16 10:55:36 +0900

    学校から教員を削除するAPIの作成

---
+++ lib/api/admin-school/delete_teacher_in_school.dart
@@ -0,0 +1,33 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:look_teacher/controller/school_crud_controller.dart';
+import 'package:look_teacher/controller/teacher_crud_controller.dart';
+import 'package:look_teacher/models/school_class_model.dart';
+import 'package:look_teacher/models/school_model.dart';
+import 'package:look_teacher/models/teacher_user_model.dart';
+
+Future<bool> deleteTeacherInSchool(
+    {required TeacherUserModel teacher,
+    required SchoolModel schoolModel,
+    required Map<String, String> schoolMap}) async {
+  final classList = [...schoolModel.schoolClassList];
+  final SchoolClassModel schoolClass = classList
+      .firstWhere((element) => element.teacherIdList.contains(teacher.uid));
+  final teacherIdList = [...schoolClass.teacherIdList..remove(teacher.uid)];
+  final newSchoolClass = schoolClass.copyWith(teacherIdList: teacherIdList);
+
+  classList
+    ..remove(schoolClass)
+    ..add(newSchoolClass);
+  log(classList.toString());
+  try {
+    await SchoolCRUDController().updateRecord(schoolModel.schoolId,
+        schoolModel.copyWith(schoolClassList: classList), schoolMap);
+    await TeacherCRUDController()
+        .updateRecord(teacher.uid, teacher.copyWith(schoolId: ''));
+    return true;
+  } on FirebaseException {
+    return false;
+  }
+}
