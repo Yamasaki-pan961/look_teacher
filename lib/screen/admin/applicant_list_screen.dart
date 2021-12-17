c883bac052cf2c8506642b0a4a076c0e2a2785a9
Yamasaki-pan961 <atusi08250522@gmail.com>
2021-12-17 19:37:12 +0900

    参加申請一覧の画面を作成

--- lib/screen/admin/applicant_list_screen.dart
+++ lib/screen/admin/applicant_list_screen.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:look_teacher/models/applicant_model.dart';
+import 'package:look_teacher/providers/current_teacher_provider.dart';
 
 class ApplicantListScreen extends HookWidget {
   const ApplicantListScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    final applicantList =
+        useProvider(teacherSchoolProvider).state!.applicantList;
     return Scaffold(
         appBar: AppBar(
           title: const Text('参加申請一覧'),
         ),
-        body: SingleChildScrollView(child: Column()));
+        body: SingleChildScrollView(
+            child: Column(
+          children: [
+            for (final applicant in applicantList)
+              ApplicantWidget(applicant: applicant)
+          ],
+        )));
   }
 }
 
@@ -20,6 +29,11 @@ class ApplicantWidget extends StatelessWidget {
   final ApplicantModel applicant;
   @override
   Widget build(BuildContext context) {
-    return Card(child: ListTile(title: Text(''),),);
+    return Card(
+      child: ListTile(
+        title: Text(applicant.teacherName),
+        subtitle: Text(applicant.message),
+      ),
+    );
   }
 }
