69264d476fe2422a63ca81444d30bb1f05eefeb0
Yamasaki-pan961 <atusi08250522@gmail.com>
2021-12-17 18:06:38 +0900

    API、inputStateを追加

--- lib/screen/admin/build_school_screen.dart
+++ lib/screen/admin/build_school_screen.dart
@@ -2,11 +2,16 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:look_teacher/api/admin-school/create_school.dart';
+import 'package:look_teacher/providers/current_teacher_provider.dart';
 
 class BuildSchoolScreen extends HookWidget {
   const BuildSchoolScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final inputState = useState<String>('');
+    final teacher = useProvider(currentTeacherProvider).state;
     return Scaffold(
         appBar: AppBar(
           title: const Text('学校の作成'),
@@ -17,26 +22,21 @@ class BuildSchoolScreen extends HookWidget {
           child: Center(
             child: Form(
               child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-                
                 TextFormField(
-                    decoration: const InputDecoration(hintText: '学校名を入力')),
-              CreateSchoolButton(buttonName: '登録'),]),
+                    decoration: const InputDecoration(hintText: '学校名を入力'),
+                    onChanged: (value) {
+                      inputState.value = value;
+                    }),
+                ElevatedButton(
+                  onPressed: () {
+                    createSchool(
+                        schoolName: inputState.value, teacher: teacher!);
+                  },
+                  child: const Text('登録'),
+                ),
+              ]),
             ),
-            
           ),
         ));
-  }
-}
-
-class CreateSchoolButton extends HookWidget {
-  const CreateSchoolButton({Key? key, required this.buttonName})
-      : super(key: key);
-  final String buttonName;
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: Text("登録"),
-    );
   }
 }
