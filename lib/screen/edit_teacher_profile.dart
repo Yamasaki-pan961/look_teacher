c858b69a4aa60f685171dc60a7e0ef68e2595463
aaarisa <b-arisa@tsuyama.kosen@ac.jp>
2021-12-17 14:12:41 +0900

    決定ボタンを追加した

--- lib/screen/edit_teacher_profile.dart
+++ lib/screen/edit_teacher_profile.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:look_teacher/api/teacher/edit_teacher_profile.dart';
 import 'package:look_teacher/models/teacher_user_model.dart';
 
-class EditTeacherProfile extends HookWidget{
-  const EditTeacherProfile({Key? key,required this.teacher}) : super(key: key);
+class EditTeacherProfile extends HookWidget {
+  const EditTeacherProfile({Key? key, required this.teacher}) : super(key: key);
   final TeacherUserModel teacher;
 
-   @override
+  @override
   Widget build(BuildContext context) {
     final inputState = useState<String>('');
     final isBluetoothState = useState<bool>(teacher.isEnableBluetooth);
@@ -17,23 +18,30 @@ class EditTeacherProfile extends HookWidget{
         centerTitle: true,
       ),
       body: Padding(
-        padding:const EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child: Center(
-          child: 
-          Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-            TextField(onChanged:(value) {
+            child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          TextField(
+            onChanged: (value) {
               inputState.value = value;
             },
-              decoration: const InputDecoration(
-                hintText: 'Enter a new name'
-              ),
-            ),
-
-            Switch(onChanged: (bool value) { isBluetoothState.value = value;}, value: isBluetoothState.value,
-            ),
-            
-
-          ])),
+            decoration: const InputDecoration(hintText: 'Enter a new name'),
+          ),
+          Switch(
+            onChanged: (bool value) {
+              isBluetoothState.value = value;
+            },
+            value: isBluetoothState.value,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                editTeacherProfile(
+                    newTeacherProfile: teacher.copyWith(
+                        name: inputState.value,
+                        isEnableBluetooth: isBluetoothState.value));
+              },
+              child: const Text('決定'))
+        ])),
       ),
     );
   }
