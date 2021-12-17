f3a6217a4b2b9c65797b1b7c464ad3bfbab9f23d
Yamasaki-pan961 <atusi08250522@gmail.com>
2021-12-17 10:58:33 +0900

    学校に参加申請を送るAPIの作成

---
+++ lib/api/teacher/send_school_join.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:look_teacher/controller/school_crud_controller.dart';
+import 'package:look_teacher/models/applicant_model.dart';
+import 'package:look_teacher/models/school_model.dart';
+import 'package:look_teacher/models/teacher_user_model.dart';
+
+Future<bool> sendSchoolJoin(
+    {required SchoolModel targetSchool,
+    required String message,
+    required TeacherUserModel teacher,
+    required Map<String, String> schoolMap}) async {
+  final applicant = ApplicantModel(teacherId: teacher.uid, message: message);
+  final newApplicantList = [...targetSchool.applicantList, applicant];
+  try {
+    await SchoolCRUDController().updateRecord(targetSchool.schoolId,
+        targetSchool.copyWith(applicantList: newApplicantList), schoolMap);
+    return true;
+  } on FirebaseException {
+    return false;
+  }
+}
