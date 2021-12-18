d172205926959339f7a0b07a56d0041b798cf3d8
Yamasaki-pan961 <atusi08250522@gmail.com>
2021-12-18 16:08:31 +0900

    変更に応じて更新されるように変更

--- lib/providers/teacher_list_provider.dart
+++ lib/providers/teacher_list_provider.dart
@@ -1,19 +1,30 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:look_teacher/controller/teacher_crud_controller.dart';
 import 'package:look_teacher/models/teacher_user_model.dart';
+import 'package:look_teacher/providers/current_teacher_provider.dart';
 
-final teacherIdListState = StateProvider.autoDispose((ref) => <String>[]);
+final classNameState = StateProvider((ref) => '');
 
-final _teacherStreamProvider = StreamProvider.autoDispose((ref) {
-  final List<String> teacherIdList = ref.watch(teacherIdListState).state;
+final _teacherIdListState = StateProvider<List<String>>((ref) {
+  final school = ref.watch(teacherSchoolProvider).state;
+  if (school != null) {
+    final className = ref.watch(classNameState).state;
+    final schoolClass = school.schoolClassList
+        .firstWhere((element) => element.name == className);
+    return schoolClass.teacherIdList;
+  }
+  return [];
+});
+
+final _teacherStreamProvider = StreamProvider((ref) {
+  final List<String> teacherIdList = ref.watch(_teacherIdListState).state;
   return TeacherCRUDController()
       .targetCollectionReference
-      .where('uid', whereIn: teacherIdList)
+      .where('uid', whereIn: teacherIdList.isNotEmpty ? teacherIdList : ['a'])
       .snapshots();
 });
 
-final teacherListProvider =
-    StateProvider.autoDispose<List<TeacherUserModel>?>((ref) {
+final teacherListProvider = StateProvider<List<TeacherUserModel>?>((ref) {
   return ref.watch(_teacherStreamProvider).when(
         data: (snapshot) {
           final teacherList = <TeacherUserModel>[];
