0428eb2ac082b40c4baacf949759a1529bb75fbc
Yamasaki-pan961 <atusi08250522@gmail.com>
2022-01-21 13:20:19 +0900

    widgetをCardとListTileで構成するように変更

--- lib/widget/teacher_widget.dart
+++ lib/widget/teacher_widget.dart
@@ -11,14 +11,14 @@ import 'package:look_teacher/providers/favorite_provider.dart';
 
 class TeacherWidget extends HookWidget {
   TeacherWidget({Key? key, required this.teacherUserModel}) : super(key: key) {
-    teacherSchoolStream = StreamProvider<DocumentSnapshot>((ref) {
+    teacherSchoolStreamProvider = StreamProvider<DocumentSnapshot>((ref) {
       return SchoolCRUDController()
           .targetCollectionReference
           .doc(teacherUserModel.schoolId)
           .snapshots();
     });
-    teacherSchool = StateProvider<SchoolModel?>((ref) {
-      return ref.watch(teacherSchoolStream).when(
+    teacherSchoolProvider = StateProvider<SchoolModel?>((ref) {
+      return ref.watch(teacherSchoolStreamProvider).when(
             data: (value) {
               return SchoolModel.fromDoc(value);
             },
@@ -28,8 +28,8 @@ class TeacherWidget extends HookWidget {
     });
   }
   final TeacherUserModel teacherUserModel;
-  late final StreamProvider<DocumentSnapshot> teacherSchoolStream;
-  late final StateProvider<SchoolModel?> teacherSchool;
+  late final StreamProvider<DocumentSnapshot> teacherSchoolStreamProvider;
+  late final StateProvider<SchoolModel?> teacherSchoolProvider;
 
   @override
   Widget build(BuildContext context) {
@@ -38,50 +38,59 @@ class TeacherWidget extends HookWidget {
     bool isFavorite = false;
     isFavorite = favoriteTeacher.contains(teacherUserModel.uid);
 
-    final SchoolModel? school = useProvider(teacherSchool).state;
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.blue, width: 2),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-        child: school != null
-            ? Column(
+    final SchoolModel? school = useProvider(teacherSchoolProvider).state;
+    return Card(
+      child: school != null
+          ? ListTile(
+              title: Row(
                 children: [
-                  Row(
-                    children: [
-                      IconButton(
-                          onPressed: () async {
-                            if (isFavorite) {
-                              await context
-                                  .read(favoriteTeacherIdListProvider.notifier)
-                                  .remove([teacherUserModel.uid]);
-                            } else {
-                              await context
-                                  .read(favoriteTeacherIdListProvider.notifier)
-                                  .add([teacherUserModel.uid]);
-                            }
-                          },
-                          icon: isFavorite
-                              ? const Icon(
-                                  Icons.star,
-                                  color: Colors.yellow,
-                                )
-                              : const Icon(Icons.star_border)),
-                      Text(teacherUserModel.name),
-                    ],
-                  ),
-                  Text(school.deviceList
-                      .firstWhere((element) =>
-                          element.deviceId == teacherUserModel.deviceId)
-                      .locationName),
-                  Text(teacherUserModel.lastScanTime.toString()),
-                  Text(school.schoolName),
+                  IconButton(
+                      onPressed: () async {
+                        if (isFavorite) {
+                          await context
+                              .read(favoriteTeacherIdListProvider.notifier)
+                              .remove([teacherUserModel.uid]);
+                        } else {
+                          await context
+                              .read(favoriteTeacherIdListProvider.notifier)
+                              .add([teacherUserModel.uid]);
+                        }
+                      },
+                      icon: isFavorite
+                          ? const Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                            )
+                          : const Icon(Icons.star_border)),
+                  Text(teacherUserModel.name),
                 ],
-              )
-            : const CircularProgressIndicator(),
-      ),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Builder(builder: (context) {
+                    final locationName = () {
+                      final list = school.deviceList
+                          .where((element) =>
+                              element.deviceId == teacherUserModel.deviceId)
+                          .toList();
+                      if (list.isNotEmpty) {
+                        return school.deviceList
+                            .firstWhere((element) =>
+                                element.deviceId == teacherUserModel.deviceId)
+                            .locationName;
+                      } else {
+                        return '不明';
+                      }
+                    }();
+                    return Text('場所: $locationName');
+                  }),
+                  Text('最終スキャン: ${teacherUserModel.lastScanTime}'),
+                  Text('所属: ${school.schoolName}'),
+                ],
+              ),
+            )
+          : const CircularProgressIndicator(),
     );
   }
 }
