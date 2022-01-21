eeb68cafc98f32231a3c2b99d5447b16b6a7d45d
Yamasaki-pan961 <atusi08250522@gmail.com>
2022-01-21 11:36:16 +0900

    クラス変数にteacherの所属する学校のプロバイダーを追加

--- lib/widget/teacher_widget.dart
+++ lib/widget/teacher_widget.dart
@@ -1,18 +1,40 @@
 import 'dart:developer';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:look_teacher/controller/school_crud_controller.dart';
+import 'package:look_teacher/models/school_model.dart';
 import 'package:look_teacher/models/teacher_user_model.dart';
 import 'package:look_teacher/providers/favorite_provider.dart';
 
 class TeacherWidget extends HookWidget {
-  const TeacherWidget({Key? key, required this.teacherUserModel})
-      : super(key: key);
+  TeacherWidget({Key? key, required this.teacherUserModel}) : super(key: key) {
+    teacherSchoolStream = StreamProvider<DocumentSnapshot>((ref) {
+      return SchoolCRUDController()
+          .targetCollectionReference
+          .doc(teacherUserModel.schoolId)
+          .snapshots();
+    });
+    teacherSchool = StateProvider<SchoolModel?>((ref) {
+      return ref.watch(teacherSchoolStream).when(
+            data: (value) {
+              return SchoolModel.fromDoc(value);
+            },
+            loading: () {},
+            error: (error, stackTrace) {},
+          );
+    });
+  }
   final TeacherUserModel teacherUserModel;
+  late final StreamProvider<DocumentSnapshot> teacherSchoolStream;
+  late final StateProvider<SchoolModel?> teacherSchool;
+  
   @override
   Widget build(BuildContext context) {
-    final List<String> favoriteTeacher = useProvider(favoriteTeacherIdListProvider);
+    final List<String> favoriteTeacher =
+        useProvider(favoriteTeacherIdListProvider);
     bool isFavorite = false;
     isFavorite = favoriteTeacher.contains(teacherUserModel.uid);
     return Container(
