5a265afcb1f3da90c507c32e7575d8dfd112002d
Yamasaki-pan961 <atusi08250522@gmail.com>
2022-01-21 15:01:27 +0900

    お気に入りの学校を表示する画面を作成

--- lib/screen/school/favorite_school_list_screen.dart
+++ lib/screen/school/favorite_school_list_screen.dart
@@ -1,32 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:look_teacher/common/push_page.dart';
 import 'package:look_teacher/providers/favorite_provider.dart';
 import 'package:look_teacher/providers/schools_provider.dart';
+import 'package:look_teacher/screen/school/school_class_list_screen.dart';
 
 class FavoriteSchoolListScreen extends HookWidget {
   FavoriteSchoolListScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    final favoriteSchoolMap = useProvider(favoriteSchoolMapProvider).state; 
-    return SingleChildScrollView(
-      child: Column(
-        children: favoriteSchoolButtonList(favoriteSchoolMap , context),
+    final favoriteSchoolMap = useProvider(favoriteSchoolMapProvider).state;
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: SingleChildScrollView(
+        child: Column(
+          children: favoriteSchoolButtonList(favoriteSchoolMap, context),
+        ),
       ),
     );
   }
 
   List<Widget> favoriteSchoolButtonList(
-      Map<String, String> favoriteSchoolMap, BuildContext context){
+      Map<String, String> favoriteSchoolMap, BuildContext context) {
     final list = <Widget>[];
 
-    favoriteSchoolMap.forEach((key, value) {
-      list.add(MaterialButton(
-        onPressed: (){
-          context.read(selectedSchoolIdProvider).state = key;
-        },
-        child: Text(value),
+    favoriteSchoolMap.forEach((schoolId, schoolName) {
+      list.add(Card(
+        child: MaterialButton(
+          onPressed: () {
+            context.read(selectedSchoolIdProvider).state = schoolId;
+            pushPage(context, const SchoolClassListScreen());
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              HookBuilder(builder: (context) {
+                final favoriteSchoolIdList =
+                    useProvider(favoriteSchoolIdListProvider);
+                final isFavorite = favoriteSchoolIdList.contains(schoolId);
+                return IconButton(
+                    onPressed: () async {
+                      if (isFavorite) {
+                        await context
+                            .read(favoriteSchoolIdListProvider.notifier)
+                            .remove([schoolId]);
+                      } else {
+                        await context
+                            .read(favoriteSchoolIdListProvider.notifier)
+                            .add([schoolId]);
+                      }
+                    },
+                    icon: isFavorite
+                        ? const Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                          )
+                        : const Icon(Icons.star_border));
+              }),
+              Text(schoolName),
+            ],
+          ),
+        ),
       ));
     });
 
