af1b39194a6542602a641b3257c03879cf8c5d15
YugeOnsen <rinta723@gmail.com>
2022-01-21 15:07:38 +0900

    脱退の設定をした

--- lib/screen/school/school_class_list_screen.dart
+++ lib/screen/school/school_class_list_screen.dart
@@ -4,9 +4,12 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:look_teacher/api/admin-school/delete_teacher_in_school.dart';
+import 'package:look_teacher/api/teacher/send_school_join.dart';
 import 'package:look_teacher/common/push_page.dart';
 import 'package:look_teacher/models/school_class_model.dart';
 import 'package:look_teacher/models/school_model.dart';
+import 'package:look_teacher/providers/current_teacher_provider.dart';
 import 'package:look_teacher/providers/schools_provider.dart';
 import 'package:look_teacher/screen/school/school_teachers_screen.dart';
 
@@ -15,10 +18,34 @@ class SchoolClassListScreen extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final loginTeacher = useProvider(currentTeacherProvider).state;
     final school = useProvider(selectedSchoolProvider).state;
     return Scaffold(
         appBar: AppBar(
           title: const Text('クラス一覧'),
+          actions: [
+            if (loginTeacher != null && loginTeacher.schoolId == '')
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  '参加',
+                  style: TextStyle(color: Colors.yellow),
+                ),
+              ),
+            if (loginTeacher != null &&
+                loginTeacher.schoolId == school?.schoolId)
+              ElevatedButton(
+                  onPressed: () {
+                    deleteTeacherInSchool(
+                        teacher: loginTeacher,
+                        schoolModel: school!,
+                        schoolMap: useProvider(schoolMapProvider).state!);
+                  },
+                  child: const Text(
+                    '脱退',
+                    style: TextStyle(color: Colors.red),
+                  )),
+          ],
         ),
         body: Padding(
             padding: const EdgeInsets.all(20),
